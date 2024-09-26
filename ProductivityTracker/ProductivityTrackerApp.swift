a44a38fddb0bbc8a1dc9bc22a2e8100a61fee6f7
Ashrith Reddy <5827632+ashrithssreddy@users.noreply.github.com>
2024-09-25 19:21:31 -0500

    Initial Commit

---
+++ ProductivityTracker/ProductivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityTrackerApp.swift
+//  ProductivityTracker
+//
+//  Created by Ashrith  Reddy on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProductivityTrackerTests/ProductivityTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityTrackerTests.swift
+//  ProductivityTrackerTests
+//
+//  Created by Ashrith  Reddy on 9/25/24.
+//
+
+import Testing
+@testable import ProductivityTracker
+
+struct ProductivityTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
